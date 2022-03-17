063ebe1988583beea29b1b5a56445cc0879d9e8a
yuanxing <yuanxing@kylinos.com.cn>
2022-03-17 14:46:30 +0800

    fearture:add password encryption store function - 添加密码加密存储功能

--- client/src/pages/user/login.h
+++ client/src/pages/user/login.h
@@ -2,8 +2,11 @@
 #define LOGIN_H
 
 #include <kiranwidgets-qt5/kiran-titlebar-window.h>
+#include <QSettings>
 #include <QWidget>
 #include "common/info-worker.h"
+
+#define SCMC_DES_KEY "vaksscms"
 
 namespace Ui
 {
@@ -19,10 +22,21 @@ public:
     explicit Login(QWidget *parent = nullptr);
     ~Login();
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
 private:
+    void initUI();
+    void loadConfig();
+    void readConfig(QString key, QString &value);
+    void writeConfig(QString key, QString value);
     bool inspectLoginParam();
+    static std::string desEncrypt(const std::string &message, const std::string &key = SCMC_DES_KEY);
+    static std::string desDecrypt(const std::string &message, const std::string &key = SCMC_DES_KEY);
 
 private slots:
+    void getPasswd();
+    void onBtnRemember(bool checked);
     void onLogin();
     void onLogout();
     void getLoginResult(const QPair<grpc::Status, user::LoginReply> &);
@@ -31,6 +45,9 @@ private slots:
 private:
     Ui::Login *ui;
     MainWindow *m_mainWindow;
+    bool m_isRemember = false;
+    QTimer *m_timer;
+    QSettings *m_loginSettings;
 };
 
 #endif  // LOGIN_H
