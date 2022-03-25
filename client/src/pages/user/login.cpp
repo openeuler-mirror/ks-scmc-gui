4bfbf5382683f696b87a5b596ec21773accfda91
yuanxing <yuanxing@kylinos.com.cn>
2022-03-25 15:40:36 +0800

    fix(login):add server configuration function
    - 添加服务器配置功能

--- client/src/pages/user/login.cpp
+++ client/src/pages/user/login.cpp
@@ -3,6 +3,7 @@
 #include <cryptopp/des.h>
 #include <cryptopp/modes.h>
 #include <kiran-log/qt5-log-i.h>
+#include <kiranwidgets-qt5/kiran-message-box.h>
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QFile>
@@ -14,8 +15,8 @@
 #include "main-window.h"
 #include "ui_login.h"
 
-//#define LOGIN_CONFIG "usr/share/ks-scmc-gui/login.ini"
-#define LOGIN_CONFIG "login.ini"
+//#define LOGIN_CONFIG "usr/share/ks-scmc-gui/user.ini"
+#define LOGIN_CONFIG "user.ini"
 #define TIMEOUT 300
 
 using namespace CryptoPP;
@@ -34,8 +35,10 @@ Login::Login(QWidget *parent) : KiranTitlebarWindow(parent),
     {
         g_server_addr = args[1].toStdString();
     }
+    m_serverConfig = new ServerConfiguration(this);
+    m_serverConfig->hide();
 
-    m_loginSettings = new QSettings(QSettings::IniFormat, QSettings::UserScope, "kylinsec", "ks-scmc-gui");
+    m_loginSettings = new QSettings(QSettings::IniFormat, QSettings::UserScope, "kylinsec", "ks-scmc-user");
     m_timer = new QTimer(this);
     connect(m_timer, &QTimer::timeout, this, &Login::getPasswd);
 
@@ -97,12 +100,10 @@ void Login::initUI()
 
     ui->lineEdit_passwd->setEchoMode(QLineEdit::Password);
     ui->lineEdit_passwd->setPlaceholderText(tr("Please input password"));
-    ui->lineEdit_server->setPlaceholderText(tr("Please input server address"));
     ui->lineEdit_username->setPlaceholderText(tr("Please input user name"));
 
     ui->lineEdit_username->setText("test");
     ui->lineEdit_passwd->setText("12345678");
-    ui->lineEdit_server->setText("10.200.12.181:10060");
 
     connect(ui->btn_login, &QPushButton::clicked, this, &Login::onLogin);
     connect(ui->radio_btn_remember_pw, &QRadioButton::toggled, this, &Login::onBtnRemember);
@@ -165,12 +166,13 @@ bool Login::inspectLoginParam()
         ui->lab_tips->setText(tr("Please enter a password with at least 8 digits!"));
         return false;
     }
-
-    //    if (ui->lineEdit_server->text().isEmpty())
-    //    {
-    //        ui->lab_tips->setText(tr("Please enter a server address!"));
-    //        return false;
-    //    }
+    m_server = m_serverConfig->getServerInfo();
+    if (m_server.isEmpty())
+    {
+        ui->lab_tips->setText(tr("Please config a server address!"));
+        m_serverConfig->show();
+        return false;
+    }
     return true;
 }
 
@@ -229,11 +231,8 @@ void Login::onMenuTrigger(QAction *act)
 {
     if (act->text() == tr("Config server"))
     {
-        if (!m_serverConfig)
-        {
-            m_serverConfig = new ServerConfiguration();
-            m_serverConfig->show();
-        }
+        m_serverConfig->show();
+        m_serverConfig->setServerInfo();
     }
 }
 
@@ -243,7 +242,7 @@ void Login::onLogin()
         return;
 
     connect(&InfoWorker::getInstance(), &InfoWorker::loginFinished, this, &Login::getLoginResult);
-    g_server_addr = ui->lineEdit_server->text().toStdString();
+    g_server_addr = m_server.toStdString();
     InfoWorker::getInstance().login(ui->lineEdit_username->text().toStdString(), ui->lineEdit_passwd->text().toStdString());
     QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));  //等待旋转
 }
@@ -279,14 +278,12 @@ void Login::getLoginResult(const QPair<grpc::Status, user::LoginReply> &reply)
             QString encryptedPw = QString::fromStdString(desEncrypt(ui->lineEdit_passwd->text().toStdString()));
             writeConfig("username", ui->lineEdit_username->text());
             writeConfig("password", encryptedPw);
-            writeConfig("server", ui->lineEdit_server->text());
         }
     }
     else
     {
         ui->lab_tips->setText(tr("Login failed! %1").arg(reply.first.error_message().data()));
         ui->lineEdit_passwd->clear();
-        ui->lineEdit_server->clear();
     }
 }
 
@@ -314,17 +311,39 @@ void Login::getLogoutResult(const QPair<grpc::Status, user::LogoutReply> &reply)
     }
 }
 
-ServerConfiguration::ServerConfiguration(QWidget *parent) : KiranTitlebarWindow(parent)
+ServerConfiguration::ServerConfiguration(QWidget *parent) : KiranTitlebarWindow(parent),
+                                                            m_ipLineEdit(nullptr),
+                                                            m_portLineEdit(nullptr),
+                                                            m_serverSetting(nullptr)
 {
+    m_serverSetting = new QSettings(QSettings::IniFormat, QSettings::UserScope, "kylinsec", "ks-scmc-server");
     initUI();
+    setServerInfo();
 }
 
 ServerConfiguration::~ServerConfiguration()
 {
+    if (m_serverSetting)
+    {
+        delete m_serverSetting;
+        m_serverSetting = nullptr;
+    }
+}
+
+QString ServerConfiguration::getServerInfo()
+{
+    //10.200.12.181:10060
+    QString server;
+    if (!m_ip.isEmpty() && !m_port.isEmpty())
+        server = m_ip + ":" + m_port;
+    else
+        server = "";
+    return server;
 }
 
 void ServerConfiguration::ServerConfiguration::initUI()
 {
+    setWindowModality(Qt::WindowModal);
     setTitle(tr("Config Server"));
     setButtonHints(TitlebarButtonHint::TitlebarCloseButtonHint);
     this->setFixedSize(400, 300);
@@ -339,22 +358,22 @@ void ServerConfiguration::ServerConfiguration::initUI()
     ipLayout->setMargin(0);
     ipLayout->setSpacing(10);
     QLabel *ipLab = new QLabel(tr("Ip Address"), ipWidget);
-    QLineEdit *ipLineEdit = new QLineEdit(ipWidget);
-    ipLineEdit->setFixedHeight(32);
-    ipLineEdit->setPlaceholderText(tr("Please input server ip address"));
+    m_ipLineEdit = new QLineEdit(ipWidget);
+    m_ipLineEdit->setFixedHeight(32);
+    m_ipLineEdit->setPlaceholderText(tr("Please input server ip address"));
     ipLayout->addWidget(ipLab);
-    ipLayout->addWidget(ipLineEdit);
+    ipLayout->addWidget(m_ipLineEdit);
 
     QWidget *portWidget = new QWidget(windowContentWidget);
     QVBoxLayout *portLayout = new QVBoxLayout(portWidget);
     portLayout->setMargin(0);
     portLayout->setSpacing(10);
     QLabel *portLab = new QLabel(tr("Port"), portWidget);
-    QLineEdit *portLineEdit = new QLineEdit(portWidget);
-    portLineEdit->setFixedHeight(32);
-    portLineEdit->setPlaceholderText(tr("Please input port"));
+    m_portLineEdit = new QLineEdit(portWidget);
+    m_portLineEdit->setFixedHeight(32);
+    m_portLineEdit->setPlaceholderText(tr("Please input port"));
     portLayout->addWidget(portLab);
-    portLayout->addWidget(portLineEdit);
+    portLayout->addWidget(m_portLineEdit);
 
     QHBoxLayout *btnLayout = new QHBoxLayout();
     QPushButton *confirmBtn = new QPushButton(tr("Confirm"));
@@ -366,4 +385,58 @@ void ServerConfiguration::ServerConfiguration::initUI()
     mainVLayout->addWidget(portWidget);
     mainVLayout->addStretch();
     mainVLayout->addLayout(btnLayout);
+
+    connect(confirmBtn, &QPushButton::clicked,
+            [this] {
+                if (m_ipLineEdit->text().isEmpty() || m_portLineEdit->text().isEmpty())
+                {
+                    KiranMessageBox::message(nullptr,
+                                             tr("Failed"),
+                                             tr("Please input complete server information!"),
+                                             KiranMessageBox::Ok);
+                    return;
+                }
+                m_ip = m_ipLineEdit->text();
+                m_port = m_portLineEdit->text();
+                writeConfig("Ip", m_ip);
+                writeConfig("Port", m_port);
+                close();
+            });
+    connect(cancleBtn, &QPushButton::clicked,
+            [this] {
+                close();
+            });
+}
+
+void ServerConfiguration::readConfig(QString key, QString &value)
+{
+    value.clear();
+    QString group = "Server";
+    KLOG_INFO() << group;
+
+    //读取配置信息
+    value = m_serverSetting->value(group + "/" + key).toString();
+}
+
+void ServerConfiguration::writeConfig(QString key, QString value)
+{
+    QString group = "Server";
+
+    QVariant variant;
+    variant.setValue(value);
+    //将信息写入配置文件
+
+    m_serverSetting->beginGroup(group);
+    m_serverSetting->setValue(key, variant);
+    m_serverSetting->endGroup();
+}
+
+void ServerConfiguration::setServerInfo()
+{
+    m_ip.clear();
+    m_port.clear();
+    readConfig("Ip", m_ip);
+    m_ipLineEdit->setText(m_ip);
+    readConfig("Port", m_port);
+    m_portLineEdit->setText(m_port);
 }
