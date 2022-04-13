38faa5893a85b621e725bd00d695482f053a76c2
chendingjian <chendingjian@kylinos.com.cn>
2022-04-13 15:10:38 +0800

    feature(outline):Add overview page functionality.
    - 添加概述页面(#53433)
    
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

---
+++ src/pages/outline/outline-cell.cpp
@@ -0,0 +1,42 @@
+#include "outline-cell.h"
+#include "ui_outline-cell.h"
+#include "main-window.h"
+
+OutlineCell::OutlineCell(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::OutlineCell)
+{
+    is_mouse_Press = false;
+//    m_outline_view = new OutlineView(this);
+    ui->setupUi(this);
+    this->setIcon();
+    ui->frame->setStyleSheet("background: #2d2d2d;");
+}
+
+OutlineCell::~OutlineCell()
+{
+    delete ui;
+}
+
+void OutlineCell::setOutlineCellName(const QString name)
+{
+    ui->label->setText(name);
+}
+
+void OutlineCell::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event)
+    is_mouse_Press = true;
+//    emit outlineCellStep(ONUTLINE);
+    emit isPress();
+}
+
+void OutlineCell::setIcon()
+{
+    ui->cell_name->setStyleSheet("QLabel{"
+                               "background-image:url(:/images/node-manager.svg);"
+                               "background-position:left;"
+                               "background-origin:content;"
+                               "background-repeat:none;"
+                               "}");
+}
