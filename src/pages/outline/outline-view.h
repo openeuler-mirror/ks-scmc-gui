a9db7c4d8556f49be40600c6022f7aa99f17c96a
chendingjian <chendingjian@kylinos.com.cn>
2022-04-13 15:10:38 +0800

    feature(outline):Add overview page functionality.
    - 添加概述页面(#53433)
    
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

---
+++ src/pages/outline/outline-view.h
@@ -0,0 +1,68 @@
+#ifndef OUTLINEVIEW_H
+#define OUTLINEVIEW_H
+
+#include <QWidget>
+#include <QFrame>
+#include <QHBoxLayout>
+#include <QScrollArea>
+#include <QScrollBar>
+#include "base/info-worker.h"
+#include "outline-cell.h"
+#include "outline-introduction.h"
+
+class OutlineCell;
+class OutlineIntroduction;
+class OutlineView : public QFrame
+{
+    Q_OBJECT
+public:
+    explicit OutlineView(QWidget *parent = nullptr);
+    ~OutlineView() override;
+    void updateInfo();
+
+protected:
+//    bool eventFilter(QObject *watched, QEvent *event);
+//    virtual void resizeEvent(QResizeEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    void initUI();
+    void initConnect();
+    QWidget *getScrollCenterWidget(); //获取widget
+    void setOutlineCellNode(); //初始化概述页面的cell
+    void setOutlineCellContainer();
+    void setOutlineCellImage();
+    void setOutlineCellTemplateContainer();
+    void setOutlineCellExamine();
+    void setOutlineCellWarning();
+
+private slots:
+    void getOutlineCellNodeNums(const QPair<grpc::Status, node::ListReply> &reply);
+    void getOutlineCellContainerNums(const QPair<grpc::Status, container::ListReply> &reply);
+    void getOutlineCellImageNums(const QPair<grpc::Status, image::ListDBReply> &reply);
+    void getOutlineCellTemplateContainerNums();
+    void getOutlineCellExamineNums();
+    void getOutlineCellWarningNums();
+
+    void emitOutlineCellType();
+    void getlNodeList();
+    void getContainerList();
+    void getImageList();
+
+signals:
+    void outlineCellStepPages(OutlineCellType type);
+
+private:
+    OutlineCell *m_outlineCell_node;
+    OutlineCell *m_outlineCell_container;
+    OutlineCell *m_outlineCell_image;
+    OutlineCell *m_outlineCell_template_container;
+    OutlineCell *m_outlineCell_examine;
+    OutlineCell *m_outlineCell_warning;
+    OutlineIntroduction *m_outlineIntroduction;
+    QScrollArea  *m_scrollArea;//滚动区域
+    QWidget      *m_scrollWidget;//滚动区域的窗口
+    QHBoxLayout  *m_layout;//水平布局滚动区域
+};
+
+#endif // OUTLINEVIEW_H
