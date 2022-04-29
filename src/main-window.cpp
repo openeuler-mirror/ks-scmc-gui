a94eca2898f7a2db267a9f2ba0952bf9b7d1f8a6
chendingjian <chendingjian@kylinos.com.cn>
2022-04-29 10:19:34 +0800

    feature(outline):Add outline jump to the page to be approve. - 新增概述页面跳转审计页面，获取待审核数据。
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- src/main-window.cpp
+++ src/main-window.cpp
@@ -20,6 +20,7 @@
 #include "pages/node/node-page-manager.h"
 #include "pages/user/passwd-update-dialog.h"
 #include "table-page.h"
+#include "pages/audit/audit-list/audit-list-page.h"
 
 #define GENERAL_OUTLINE QObject::tr("General Outline")
 #define CONTAINER_MANAGER QObject::tr("Container Manager")
@@ -203,6 +204,7 @@ void MainWindow::initUI()
 
     //pageMap.value
     const QMap<GUIDE_ITEM, QString> pageMap = {
+        {GUIDE_ITEM_AUDIT_APPLY_LIST, AUDIT_APPLY_LIST},
         {GUIDE_ITEM_CONTAINER_LIST, CONTAINER_LIST},
         {GUIDE_ITEM_NODE_MANAGER, NODE_MANAGER},
         {GUIDE_ITEM_IMAGE_LIST, IMAGE_MANAGER}};
@@ -265,14 +267,23 @@ void MainWindow::initUI()
     connect(ui->listWidget, &QListWidget::itemClicked, this, &MainWindow::onItemClicked);
 }
 
-void MainWindow::outlinePageChange(int type, QString str)
+void MainWindow::outlinePageChange(QString str)
 {
-    m_stackedWidget->setCurrentWidget(m_pageMap.value(str));
-    ui->listWidget->setCurrentRow(type);
-    auto item = ui->listWidget->item(type);
-    GuideItem* guideItem = qobject_cast<GuideItem*>(ui->listWidget->itemWidget(item));
-    guideItem->setSelected(true);
-    m_pageMap[str]->updateInfo();
+    setPageName(str);
+    QListWidgetItem *currItem = new QListWidgetItem;
+
+    for (int i = 0;i < ui->listWidget->count() ; i++) {
+        currItem = ui->listWidget->item(i);
+        if(currItem->data(Qt::UserRole).toString() == str)
+        {
+            GuideItem* guideItem = qobject_cast<GuideItem*>(ui->listWidget->itemWidget(currItem));
+            m_stackedWidget->setCurrentWidget(m_pageMap.value(str));
+            guideItem->setSelected(true);
+            ui->listWidget->setItemSelected(currItem,true);
+            m_pageMap[str]->updateInfo();
+            break;
+        }
+    }
 }
 
 void MainWindow::outlineJumpPage(OutlineCellType type)
@@ -284,19 +295,19 @@ void MainWindow::outlineJumpPage(OutlineCellType type)
     {
     case ONUTLINE_CELL_NODE:
     {
-        outlinePageChange(9, NODE_MANAGER);
+        outlinePageChange(NODE_MANAGER);
         outlineItem->setSelected(false);
         break;
     }
     case ONUTLINE_CELL_CONTAINER:
     {
-        outlinePageChange(6, CONTAINER_LIST);
+        outlinePageChange(CONTAINER_LIST);
         outlineItem->setSelected(false);
         break;
     }
     case ONUTLINE_CELL_IMAGE:
     {
-        outlinePageChange(8, IMAGE_MANAGER);
+        outlinePageChange(IMAGE_MANAGER);
         outlineItem->setSelected(false);
         break;
     }
@@ -306,6 +317,8 @@ void MainWindow::outlineJumpPage(OutlineCellType type)
     }
     case ONUTLINE_CELL_EXAMINE:
     {
+        outlinePageChange(AUDIT_APPLY_LIST);
+        outlineItem->setSelected(false);
         break;
     }
     case ONUTLINE_CELL_NODE_WARNING:
@@ -330,6 +343,7 @@ Page* MainWindow::createSubPage(GUIDE_ITEM itemEnum)
     }
     case GUIDE_ITEM_CONTAINER_TEMPLATE_LIST:
     {
+        break;
     }
     case GUIDE_ITEM_NODE_MANAGER:
     {
@@ -340,6 +354,11 @@ Page* MainWindow::createSubPage(GUIDE_ITEM itemEnum)
     case GUIDE_ITEM_IMAGE_LIST:
     {
         page = new ImageListPage(this);
+        break;
+    }
+    case GUIDE_ITEM_AUDIT_APPLY_LIST:
+    {
+        page = new AuditListPage(this);
         break;
     }
     default:
