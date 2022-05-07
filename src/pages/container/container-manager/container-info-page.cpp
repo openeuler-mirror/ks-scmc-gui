64851d7a562a743655cdc3dcefad1634e322b90c
yuanxing <yuanxing@kylinos.com.cn>
2022-05-07 17:41:39 +0800

    fix(container):add container info when user clicked container name - 当用户点击容器名时添加容器信息界面

---
+++ src/pages/container/container-manager/container-info-page.cpp
@@ -0,0 +1,38 @@
+#include "container-info-page.h"
+#include <kiran-log/qt5-log-i.h>
+#include "monitor-content.h"
+ContainerInfoPage::ContainerInfoPage(QWidget *parent) : TabPage(parent), m_monitor(nullptr)
+{
+    createSubPage(CONTAINER_INFO_SUB_PAGE_TYPE_MONITOR);
+    createSubPage(CONTAINER_INFO_SUB_PAGE_TYPE_BACKUP);
+}
+
+void ContainerInfoPage::setContainerInfo(QMap<QString, QVariant> infoMap)
+{
+    m_infoMap = infoMap;
+}
+
+void ContainerInfoPage::updateInfo(QString keyword)
+{
+    KLOG_INFO() << "ContainerInfoPage UpdateInfo";
+    m_monitor->updateMonitorInfo(m_infoMap.value(NODE_ID).toInt(), m_infoMap.value(CONTAINER_ID).toString().toStdString());
+}
+
+void ContainerInfoPage::createSubPage(ContainerInfoSubPageType type)
+{
+    switch (type)
+    {
+    case CONTAINER_INFO_SUB_PAGE_TYPE_MONITOR:
+        m_monitor = new MonitorContent(this);
+        addTabPage(m_monitor, tr("Monitor"));
+        break;
+    default:
+        break;
+    }
+}
+
+void ContainerInfoPage::updatePageInfo(int index)
+{
+    if (index == CONTAINER_INFO_SUB_PAGE_TYPE_MONITOR)
+        m_monitor->updateMonitorInfo(m_infoMap.value(NODE_ID).toInt(), m_infoMap.value(CONTAINER_ID).toString().toStdString());
+}
