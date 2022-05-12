c78325021f1d7ff55db84541e7c8753794c0cc02
yuanxing <yuanxing@kylinos.com.cn>
2022-05-12 18:13:05 +0800

    fix(security):add security config when create and inspect container
    - 当创建和显示容器信息时，添加安全配置

--- src/pages/outline/outline-view.cpp
+++ src/pages/outline/outline-view.cpp
@@ -88,12 +88,6 @@ void OutlineView::getImageList()
     InfoWorker::getInstance().listDBImage();
 }
 
-void OutlineView::getlTemplateContainer()
-{
-    KLOG_INFO() << "getlTemplateContainer";
-    InfoWorker::getInstance().listTemplate();
-}
-
 QWidget *OutlineView::getScrollCenterWidget()
 {
     return m_scrollWidget;
@@ -238,16 +232,12 @@ void OutlineView::updateInfo()
     disconnect(&InfoWorker::getInstance(), &InfoWorker::listNodeFinished, 0, 0);
     disconnect(&InfoWorker::getInstance(), &InfoWorker::listContainerFinished, 0, 0);
     disconnect(&InfoWorker::getInstance(), &InfoWorker::listDBImageFinished, 0, 0);
-//    disconnect(&InfoWorker::getInstance(), &InfoWorker::listTemplateFinished, 0, 0);
     connect(&InfoWorker::getInstance(), &InfoWorker::listNodeFinished, this, &OutlineView::getOutlineCellNodeNums);
     connect(&InfoWorker::getInstance(), &InfoWorker::listContainerFinished, this, &OutlineView::getOutlineCellContainerNums);
     connect(&InfoWorker::getInstance(), &InfoWorker::listDBImageFinished, this, &OutlineView::getOutlineCellImageNums);
-    connect(&InfoWorker::getInstance(), &InfoWorker::listTemplateFinished, this, &OutlineView::getOutlineCellTemplateContainerNums);
-
     getlNodeList();
     getContainerList();
     getImageList();
-    getlTemplateContainer();
     //    m_outlineCell_image->ui->Name_counts->setText("666");
 }
 
@@ -339,14 +329,8 @@ void OutlineView::getOutlineCellImageNums(const QPair<grpc::Status, image::ListD
     getOutlineCellExamineNums(reply);
 }
 
-void OutlineView::getOutlineCellTemplateContainerNums(const QPair<grpc::Status, container::ListTemplateReply> &reply)
+void OutlineView::getOutlineCellTemplateContainerNums()
 {
-    KLOG_INFO() << "getOutlineCellTemplateContainerNums";
-    if (reply.first.ok())
-    {
-        int size = reply.second.data_size();
-        m_outlineCell_template_container->ui->Name_counts->setText(QString::number(size, 10));
-    }
 }
 
 void OutlineView::getOutlineCellExamineNums(const QPair<grpc::Status, image::ListDBReply> &reply)
