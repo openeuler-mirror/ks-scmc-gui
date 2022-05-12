45610ab95f2a63639dc1c3c3f5a4ab960cc45a1d
chendingjian <chendingjian@kylinos.com.cn>
2022-05-12 12:56:23 +0800

    feature(license):Add software activation function.
    - 新增软件激活功能，添加kylin-license依赖，生成激活码的名称定义为 KSSCMCGui
    - 修改了一些翻译
    
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- src/login-dialog.h
+++ src/login-dialog.h
@@ -2,10 +2,15 @@
 #define LOGIN_DIALOG_H
 
 #include <kiranwidgets-qt5/kiran-titlebar-window.h>
+#include <kylin-license/license-i.h>
 #include <QLineEdit>
 #include <QSettings>
 #include <QWidget>
 #include "info-worker.h"
+#include "common/license.h"
+#include "dbus/dbus-utils.h"
+#include "activate-page.h"
+#include "error-message-box.h"
 
 namespace Ui
 {
@@ -27,8 +32,10 @@ protected:
 
 private:
     void initUI();
+    void initMessageBox();
     void loadConfig();
     bool inspectLoginParam();
+    void getLicense(QString license_str);
 
 private slots:
     void onMenuTrigger(QAction *act);
@@ -37,6 +44,13 @@ private slots:
     void getLoginResult(const QPair<grpc::Status, user::LoginReply> &);
     void getLogoutResult(const QPair<grpc::Status, user::LogoutReply> &);
     void sessionExpire();
+    void updateLicense(bool);
+    void activation(QString);
+    void showActivatePage();
+    void showErrorBox();
+
+signals:
+    void sigLicenseChange();
 
 private:
     Ui::LoginDialog *ui;
@@ -44,6 +58,14 @@ private:
     QTimer *m_timer;
     ServerConfigDialog *m_serverCfgDlg;
     QString m_server;
+    ActivatePage *m_activate_page;
+    License *m_license;
+    DBusUtils *m_dbusutil;
+    ErrorMessageBox *m_errorMessageBox;
+    KiranMessageBox *m_activate_page_box;
+    KiranMessageBox *m_dbusErrorBox;
+    QLabel *activate_label;
+//    QAction *actionLicense;
 };
 
 #endif  // LOGIN_DIALOG_H
