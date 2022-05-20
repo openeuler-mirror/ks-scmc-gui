8a612810d2f1293de0424fad4134b4124db60565
chendingjian <chendingjian@kylinos.com.cn>
2022-05-20 17:05:41 +0800

    fix(audit): fix update fail. - 修复审批页面不刷新. Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- src/pages/audit/audit-list/audit-list-page.h
+++ src/pages/audit/audit-list/audit-list-page.h
@@ -25,6 +25,9 @@ private:
     void updatePageInfo(int index);
 
 private:
+    void updateAuditInfo();
+
+private:
     ImageListPage *m_imagelist_approve;
     ImageListPage *m_imagelist_passed;
     ImageListPage *m_imagelist_rejected;
