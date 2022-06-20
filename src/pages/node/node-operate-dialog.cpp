c857fa79fae1997cbf80ab1d4304e712b21c3651
yuanxing <yuanxing@kylinos.com.cn>
2022-06-20 18:02:07 +0800

    fix(node):change the titlebar to kiran and change the widget to QTextEdit of description - 修改标题栏为kiran，修改描述输入控件为QTextEdit

--- src/pages/node/node-operate-dialog.cpp
+++ src/pages/node/node-operate-dialog.cpp
@@ -1,21 +1,21 @@
 #include "node-operate-dialog.h"
+#include <widget-property-helper.h>
 #include <QRegExpValidator>
 #include "common/message-dialog.h"
 #include "def.h"
 #include "ui_node-operate-dialog.h"
-NodeOperateDialog::NodeOperateDialog(NodeOperateType type, QWidget *parent) : QWidget(parent),
+NodeOperateDialog::NodeOperateDialog(NodeOperateType type, QWidget *parent) : KiranTitlebarWindow(parent),
                                                                               ui(new Ui::NodeOperateDialog),
                                                                               m_type(type)
 {
-    ui->setupUi(this);
+    ui->setupUi(getWindowContentWidget());
     setAttribute(Qt::WA_DeleteOnClose);
     setWindowIcon(QIcon(":/images/logo.png"));
     setWindowTitle(tr("Add Node"));
     ui->lineEdit_node_name->setFocus();
     ui->lineEdit_node_name->setMaxLength(50);
     ui->lineEdit_node_name->setPlaceholderText(tr("Please input 1 to 50 characters"));
-    ui->lineEdit_node_comment->setMaxLength(200);
-    ui->lineEdit_node_comment->setPlaceholderText(tr("Please input 0 to 200 characters"));
+    ui->text_comment->setPlaceholderText(tr("Please input 0 to 200 characters"));
 
     if (type == NODE_OPERATE_TYPE_CREATE)
         setWindowTitle(tr("Add Node"));
@@ -28,8 +28,12 @@ NodeOperateDialog::NodeOperateDialog(NodeOperateType type, QWidget *parent) : QW
     QRegExp rx("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
     ui->lineEdit_node_ip->setValidator(new QRegExpValidator(rx, parent));
 
+    Kiran::WidgetPropertyHelper::setButtonType(ui->btn_save, Kiran::BUTTON_Default);
+    Kiran::WidgetPropertyHelper::setButtonType(ui->btn_cancel, Kiran::BUTTON_Normal);
     connect(ui->btn_save, &QPushButton::clicked, this, &NodeOperateDialog::onSave);
     connect(ui->btn_cancel, &QPushButton::clicked, this, &NodeOperateDialog::onCancel);
+
+    connect(ui->text_comment, &QTextEdit::textChanged, this, &NodeOperateDialog::limitLength);
 }
 
 NodeOperateDialog::~NodeOperateDialog()
@@ -42,7 +46,7 @@ void NodeOperateDialog::setNodeInfo(int64_t nodeId, QString name, QString addr, 
     m_nodeId = nodeId;
     ui->lineEdit_node_name->setText(name);
     ui->lineEdit_node_ip->setText(addr);
-    ui->lineEdit_node_comment->setText(comment);
+    ui->text_comment->setText(comment);
 }
 
 NodeOperateType NodeOperateDialog::getType()
@@ -59,7 +63,7 @@ void NodeOperateDialog::onSave()
 {
     QString name = ui->lineEdit_node_name->text();
     QString ip = ui->lineEdit_node_ip->text();
-    QString comment = ui->lineEdit_node_comment->text();
+    QString comment = ui->text_comment->toPlainText();
     if (name.isEmpty() || ip.isEmpty())
     {
         MessageDialog::message(tr("Add node"),
@@ -80,4 +84,20 @@ void NodeOperateDialog::onSave()
 void NodeOperateDialog::onCancel()
 {
     this->close();
+}
+
+void NodeOperateDialog::limitLength()
+{
+    QString textContent = ui->text_comment->toPlainText();
+    int length = textContent.count();
+    int maxLength = 200;  // 最大字符数
+    if (length > maxLength)
+    {
+        int position = ui->text_comment->textCursor().position();
+        QTextCursor textCursor = ui->text_comment->textCursor();
+        textContent.remove(position - (length - maxLength), length - maxLength);
+        ui->text_comment->setText(textContent);
+        textCursor.setPosition(position - (length - maxLength));
+        ui->text_comment->setTextCursor(textCursor);
+    }
 }
