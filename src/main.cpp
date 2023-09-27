70d3fdd0870498247612345f078c5c939e6b36b1
yuanxing <yuanxing@kylinsec.com.cn>
2023-09-27 09:06:41 +0800

    fix(main):set env of fcitx before create app - 在创建app之前设置fcitx输入法环境变量
    Fix #16301

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,9 @@
 
 int main(int argc, char *argv[])
 {
+    //设置qt程序加载输入法模块为fcitx,需要设置在创建app之前才能生效
+    qputenv("QT_IM_MODULE", QByteArray("fcitx"));
+
     //设置日志输出
     if (klog_qt5_init("", "kylinsec-session", APP_NAME, APP_NAME) < 0)
     {
@@ -41,10 +44,6 @@ int main(int argc, char *argv[])
     //设在主题不跟随系统主题变化,KY3.4-4,KY3.3-6中不需要此功能
     Kiran::StylePalette::instance()->setDesignatedPaletteType(Kiran::PALETTE_DARK);
 #endif
-    //a.setStyle(QStyleFactory::create("fusion"));
-
-    //设置qt程序加载输入法模块为fcitx
-    qputenv("QT_IM_MODULE", QByteArray("fcitx"));
 
     ///加载qss样式表
     QFile file(":/style/theme.qss");
