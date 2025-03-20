d0ba3c5f43ea8bf9d6019400f12dfd28629ebc2d
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-20 19:54:32 +0800

    fix（translations）：modify container setting - 更新翻译文件

--- translations/ks-scmc-gui.zh_CN.ts
+++ translations/ks-scmc-gui.zh_CN.ts
@@ -1241,7 +1241,7 @@ The first characters must be letter or digit</source>
     </message>
     <message>
         <location filename="../src/pages/container/container-setting.cpp" line="313"/>
-        <location filename="../src/pages/container/container-setting.cpp" line="1308"/>
+        <location filename="../src/pages/container/container-setting.cpp" line="1312"/>
         <source>Create template</source>
         <translation>创建模板</translation>
     </message>
