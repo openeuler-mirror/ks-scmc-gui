f19ded6da256b4b28e24487052f13925668766fb
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-20 19:21:01 +0800

    fix（pages）：modify code changes - 添加输入框输入限制

--- src/pages/user/server-config-dialog.cpp
+++ src/pages/user/server-config-dialog.cpp
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/pages/user/server-config-dialog.cpp
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
