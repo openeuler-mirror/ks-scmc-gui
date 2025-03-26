c2c6764c462390625fc051c10ab1553bd5f2ecf3
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-26 10:59:17 +0800

    fix（pages）：modify code changes - 在基于模板创建容器时，切换模板后更新环境变量和共享目录配置路径

--- src/pages/container/advanced-configuration/envs-conf-tab.cpp
+++ src/pages/container/advanced-configuration/envs-conf-tab.cpp
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/pages/container/advanced-configuration/envs-conf-tab.cpp
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
