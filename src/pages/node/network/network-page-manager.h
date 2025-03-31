ffb65499776584fdf05072a0ea605b3bb444cb54
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-31 08:49:41 +0800

    fix（pages）：add network page manager kylinsec co - 修复CI编译错误

--- src/pages/node/network/network-page-manager.h
+++ src/pages/node/network/network-page-manager.h
@@ -1,3 +1,10 @@
+/**
+ * @file          src/pages/node/network/network-page-manager.h
+ * @brief
+ * @author        yuanxing <yuanxing@kylinsec.com>
+ * @copyright (c) 2020 ~ 2025 KylinSec Co., Ltd.
+ */
+
 #ifndef NETWORKPAGEMANAGER_H
 #define NETWORKPAGEMANAGER_H
 
