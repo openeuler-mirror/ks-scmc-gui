9fbc0e30e28aa6150ff82818ab77355f9e131c65
yuanxing <yuanxing@kylinsec.com.cn>
2025-03-31 08:49:41 +0800

    fix（pages）：add container manager container app operate dialog kylinsec co - 修复CI编译错误

--- src/pages/container/container-manager/container-app-operate-dialog.h
+++ src/pages/container/container-manager/container-app-operate-dialog.h
@@ -1,3 +1,10 @@
+/**
+ * @file          src/pages/container/container-manager/container-app-operate-dialog.h
+ * @brief
+ * @author        yuanxing <yuanxing@kylinsec.com>
+ * @copyright (c) 2020 ~ 2025 KylinSec Co., Ltd.
+ */
+
 #ifndef CONTAINERAPPOPERATEDIALOG_H
 #define CONTAINERAPPOPERATEDIALOG_H
 
