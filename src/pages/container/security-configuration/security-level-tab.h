37ae58febc04ac4e50dfaf478efb5de333820dff
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-23 14:27:27 +0800

    feature（pages）：add security configuration security level tab kylinsec co widget - 在容器配置中添加安全模板配置

---
+++ src/pages/container/security-configuration/security-level-tab.h
@@ -0,0 +1,38 @@
+/**
+ * @file          src/pages/container/security-configuration/security-level-tab.h
+ * @brief
+ * @author        yuanxing <yuanxing@kylinsec.com>
+ * @copyright (c) 2020 ~ 2025 KylinSec Co., Ltd.
+ */
+
+#ifndef SECURITYLEVELTAB_H
+#define SECURITYLEVELTAB_H
+
+#include <QMap>
+#include <QWidget>
+
+namespace Ui
+{
+class SecurityLevelTab;
+}
+
+class SecurityLevelTab : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SecurityLevelTab(QWidget *parent = nullptr);
+    ~SecurityLevelTab();
+
+    void setSecurityInfo();
+    void getSecurityInfo();
+
+private:
+    void initUI();
+
+private:
+    Ui::SecurityLevelTab *ui;
+    QMap<QString, QString> m_securityLevelMap;
+};
+
+#endif  // SECURITYLEVELTAB_H
