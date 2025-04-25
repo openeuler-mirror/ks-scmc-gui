3683411b01a8337da46c46e25348bf6d2e994ba9
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-25 10:42:53 +0800

    feature（*）：modify object system manager user manager outline - 添加用户管理界面

--- src/main-window.cpp
+++ src/main-window.cpp
@@ -52,8 +52,8 @@
 
 #define NODE_MANAGER QObject::tr("Node Manager")
 
-// #define SYSTEM_MANAGER QObject::tr("System Manager")
-// #define OUTLINE_PAGES QObject::tr("Outline")
+#define SYSTEM_MANAGER QObject::tr("System Manager")
+#define USER_MANAGER QObject::tr("User Manager")
 
 #define TIMEOUT 200
 MainWindow::MainWindow(QString name, QString userRole, QWidget* parent)
