f37c22ceca32236f85b0dec43c34fddac09355f1
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-28 11:40:14 +0800

    feature（pages）：modify code changes - 添加网络配置和文件保护UI界面

--- src/pages/container/security-configuration/network-access-item.h
+++ src/pages/container/security-configuration/network-access-item.h
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/pages/container/security-configuration/network-access-item.h
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
