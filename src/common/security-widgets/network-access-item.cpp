d1194ad26ba6cdd72cc989a7f569cbc7870e0888
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-28 14:56:41 +0800

    feature（pages）：add advanced configuration kylinsec co combobox boxlayout - 添加IPC UI设置界面

---
+++ src/common/security-widgets/network-access-item.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file          /ks-scmc-gui/src/pages/container/security-configuration/network-access-item.cpp
+ * @brief
+ * @author        yuanxing <yuanxing@kylinos.com>
+ * @copyright (c) 2022 KylinSec. All rights reserved.
+ */
+#include "network-access-item.h"
+#include <kiran-log/qt5-log-i.h>
+#include <QRegExpValidator>
+#include "ui_network-access-item.h"
+NetworkAccessItem::NetworkAccessItem(QWidget *parent) : QWidget(parent),
+                                                        ui(new Ui::NetworkAccessItem)
+{
+    ui->setupUi(this);
+    initUI();
+}
+
+NetworkAccessItem::~NetworkAccessItem()
+{
+    delete ui;
+}
+
+void NetworkAccessItem::setDeleteBtnVisible(bool visible)
+{
+    ui->btn_delete->setVisible(visible);
+}
+
+void NetworkAccessItem::getInfo(QStringList &protocols, QString &addr, int &port)
+{
+    ui->btn_tcp->isChecked() ? protocols.append("tcp") : protocols.append("");
+    ui->btn_udp->isChecked() ? protocols.append("udp") : protocols.append("");
+    ui->btn_icmp->isChecked() ? protocols.append("icmp") : protocols.append("");
+    protocols.removeAll("");
+
+    addr = ui->lineEdit_address->text();
+    port = ui->lineEdit_port->text().toInt();
+}
+
+void NetworkAccessItem::setInfo(QStringList protocols, QString addr, int port)
+{
+    KLOG_DEBUG() << "Set network access item info:" << protocols << addr << port;
+    protocols.contains("tcp", Qt::CaseInsensitive) ? ui->btn_tcp->setChecked(true) : ui->btn_tcp->setChecked(false);
+    protocols.contains("udp", Qt::CaseInsensitive) ? ui->btn_udp->setChecked(true) : ui->btn_udp->setChecked(false);
+    protocols.contains("icmp", Qt::CaseInsensitive) ? ui->btn_icmp->setChecked(true) : ui->btn_icmp->setChecked(false);
+
+    ui->lineEdit_address->setText(addr);
+    ui->lineEdit_port->setText(QString::number(port));
+}
+
+void NetworkAccessItem::ondelete()
+{
+    emit sigDelete();
+}
+
+void NetworkAccessItem::onAdd()
+{
+    emit sigAdd();
+}
+
+void NetworkAccessItem::initUI()
+{
+    ui->btn_add->setCursor(Qt::PointingHandCursor);
+    ui->btn_delete->setCursor(Qt::PointingHandCursor);
+    ui->lineEdit_address->setPlaceholderText(tr("eg: 192.168.1.10 or 10.0.0.0/8"));
+    ui->lineEdit_address->setTextMargins(10, 0, 0, 0);
+    QRegExp rx("^[0-9.]+$");
+    ui->lineEdit_address->setValidator(new QRegExpValidator(rx, this));
+    ui->lineEdit_address->setMaxLength(20);
+
+    ui->lineEdit_port->setText(QString::number(0));
+    ui->lineEdit_port->setTextMargins(10, 0, 0, 0);
+    ui->lineEdit_port->setMaxLength(5);
+
+    ui->btn_tcp->setChecked(true);
+    connect(ui->btn_add, &QToolButton::clicked, this, &NetworkAccessItem::onAdd);
+    connect(ui->btn_delete, &QToolButton::clicked, this, &NetworkAccessItem::ondelete);
+}
---
+++ src/common/security-widgets/security-list-item.cpp
@@ -0,0 +1,129 @@
+/**
+ * @file          /ks-scmc-gui/src/pages/container/security-configuration/security-list-item.cpp
+ * @brief         
+ * @author        yuanxing <yuanxing@kylinos.com>
+ * @copyright (c) 2022 KylinSec. All rights reserved.
+ */
+#include "security-list-item.h"
+#include <kiran-log/qt5-log-i.h>
+#include <QFile>
+#include <QTimer>
+#include "ui_security-list-item.h"
+
+#define TIMEOUT 1000
+SecurityListItem::SecurityListItem(QString text, QWidget *parent) : QWidget(parent),
+                                                                    ui(new Ui::SecurityListItem),
+                                                                    m_isPathCorrect(true)
+{
+    ui->setupUi(this);
+    ui->label_name->setText(text);
+    ui->lab_error_tips->setText(tr("the path format is wrong"));
+    ui->lab_error_tips->setStyleSheet("color:#F56C6C;font-size:12px;");
+    ui->lab_error_tips->setVisible(false);
+    ui->btn_add->setCursor(Qt::PointingHandCursor);
+    ui->btn_delete->setCursor(Qt::PointingHandCursor);
+    ui->lineEdit->setTextMargins(10, 0, 0, 0);
+
+    connect(ui->btn_add, &QToolButton::clicked,
+            [this] {
+                emit sigAdd();
+            });
+    connect(ui->btn_delete, &QToolButton::clicked,
+            [this] {
+                emit sigDelete();
+            });
+    connect(ui->lineEdit, &QLineEdit::textChanged,
+            [this](QString text) {
+                ui->lineEdit->setToolTip(tooptipWordWrap(text));
+                checkPath();
+            });
+}
+
+SecurityListItem::~SecurityListItem()
+{
+    delete ui;
+}
+
+void SecurityListItem::setDeleteBtnVisible(bool visible)
+{
+    ui->btn_delete->setVisible(visible);
+}
+
+void SecurityListItem::updateItemName(QString text)
+{
+    ui->label_name->setText(text);
+}
+
+void SecurityListItem::setInfo(QString text)
+{
+    ui->lineEdit->setText(text);
+}
+
+QString SecurityListItem::getInfo()
+{
+    return ui->lineEdit->text();
+}
+
+bool SecurityListItem::getPathCorrect()
+{
+    return m_isPathCorrect;
+}
+
+void SecurityListItem::changeEvent(QEvent *event)
+{
+    if (event->type() == QEvent::EnabledChange)
+    {
+        ui->lineEdit->setCursorPosition(0);
+    }
+}
+
+void SecurityListItem::checkPath()
+{
+    QString path = ui->lineEdit->text();
+    m_isPathCorrect = true;
+    if (!path.isEmpty())
+    {
+        QRegExp regExp("^/.+$");
+        if (!regExp.exactMatch(path))
+            m_isPathCorrect = false;
+    }
+
+    ui->lab_error_tips->setVisible(!m_isPathCorrect);
+}
+
+QString SecurityListItem::tooptipWordWrap(const QString &org)
+{
+    QString result;
+    QFontMetrics fm(fontMetrics());
+    int textWidthInPxs = fm.width(org);
+    const int rear = org.length();
+    int pre = 0, vernier = 1;
+    unsigned int pickUpWidthPxs = 0;
+    QString pickUp;
+
+    if (textWidthInPxs <= width())
+    {
+        result = org;
+        return result;
+    }
+
+    while (vernier <= rear)
+    {
+        pickUp = org.mid(pre, vernier - pre);
+        pickUpWidthPxs = fm.width(pickUp);
+        if (pickUpWidthPxs >= width())
+        {
+            result += pickUp + "\n";  // 插入换行符，或者使用<br/>标签
+            pre = vernier;
+            pickUpWidthPxs = 0;
+        }
+        ++vernier;
+    }
+
+    if (pickUpWidthPxs < width() && !pickUp.isEmpty())
+    {
+        result += pickUp;
+    }
+
+    return result;
+}
