d5d90aa5e210160293bfea6ba8096e40d88324b1
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（pages）：modify info worker - 完善调用后台接口文件

--- src/pages/audit/log-setting/log-setting-view.h
+++ src/pages/audit/log-setting/log-setting-view.h
@@ -10,7 +10,7 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QWidget>
-#include "info-worker.h"
+#include "base/interface/logging.h"
 #include "page.h"
 
 class LogSettingView : public Page
