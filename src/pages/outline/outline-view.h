9392340ca40c4a826c41af63448c8f39232447a0
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（pages）：modify code changes - 完善调用后台接口文件

--- src/pages/outline/outline-view.h
+++ src/pages/outline/outline-view.h
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/pages/outline/outline-view.h
- * @brief         
+ * @brief
  * @author        chendingjian <chendingjian@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
