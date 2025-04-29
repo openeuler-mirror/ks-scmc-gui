efefe347675e69915b306cdffb8b8671ce04bdf3
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（interface）：add kylinsec co mutex object pair - 完善调用后台接口文件

---
+++ src/base/interface/node.h
@@ -0,0 +1,58 @@
+﻿/**
+ * @file          src/base/interface/node.h
+ * @brief
+ * @author        yuanxing <yuanxing@kylinsec.com>
+ * @copyright (c) 2020 ~ 2025 KylinSec Co., Ltd.
+ */
+
+#pragma once
+
+#include <QMutex>
+#include <QObject>
+#include <QPair>
+
+#include <grpcpp/grpcpp.h>
+
+#include "rpc.h"
+
+class Node : public QObject
+{
+    Q_OBJECT
+public:
+    static Node &getInstance()
+    {
+        static Node s_Node;
+        return s_Node;
+    }
+
+    static QString generateId(QObject *callObj);
+
+    // node management
+    void listNode(const QString objId);
+    void createNode(const QString objId, const node::CreateRequest &);
+    void removeNode(const QString objId, const std::vector<int64_t> &node_ids);
+    void nodeStatus(const QString objId, const std::vector<int64_t> &node_ids);
+    void updateNode(const QString objId, const node::UpdateRequest &);
+
+private:
+    Node(QObject *parent = nullptr);
+    ~Node();
+
+    // node management
+    static QPair<grpc::Status, node::ListReply> _listNode(const node::ListRequest &);
+    static QPair<grpc::Status, node::CreateReply> _createNode(const node::CreateRequest &);
+    static QPair<grpc::Status, node::RemoveReply> _removeNode(const node::RemoveRequest &);
+    static QPair<grpc::Status, node::StatusReply> _nodeStatus(const node::StatusRequest &);
+    static QPair<grpc::Status, node::UpdateReply> _updateNode(const node::UpdateRequest &);
+
+signals:
+    // node management
+    void listNodeFinished(QString objId, const QPair<grpc::Status, node::ListReply> &);
+    void createNodeFinished(const QString objId, const QPair<grpc::Status, node::CreateReply> &);
+    void removeNodeFinished(const QString objId, const QPair<grpc::Status, node::RemoveReply> &);
+    void statusNodeFinished(const QString objId, const QPair<grpc::Status, node::StatusReply> &);
+    void updateNodeFinished(const QString objId, const QPair<grpc::Status, node::UpdateReply> &);
+
+private:
+    QMutex mutex;
+};
