081aa9993e56440bab9f183892d6e15603996ca5
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（pages）：modify code changes - 完善调用后台接口文件

--- src/pages/container/template-manager/template-list-page.cpp
+++ src/pages/container/template-manager/template-list-page.cpp
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/pages/container/template-manager/template-list-page.cpp
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
