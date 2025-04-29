38454c7aa23bf9d4638cec99e8e7ccda68017f0b
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（pages）：modify ContainerListPage::getContainerList: container instance m... - 完善调用后台接口文件

--- src/pages/container/container-manager/container-list-page.cpp
+++ src/pages/container/container-manager/container-list-page.cpp
@@ -625,13 +625,13 @@ void ContainerListPage::getContainerList()
     std::vector<int64_t> vecNodeId;
     if (m_nodeId < 0)
     {
-        InfoWorker::getInstance().listContainer(m_objId, vecNodeId, true);  // 获取所有容器
+        Container::getInstance().listContainer(m_objId, vecNodeId, true);  // 获取所有容器
     }
     else
     {
         KLOG_DEBUG() << "Get container list of node " << m_nodeId;
         vecNodeId.push_back(m_nodeId);
-        InfoWorker::getInstance().listContainer(m_objId, vecNodeId, true);  // 获取某节点下的容器
+        Container::getInstance().listContainer(m_objId, vecNodeId, true);  // 获取某节点下的容器
     }
 }
 
