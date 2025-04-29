5e28ce5264fec9b0f4564eb0a5efb7ce00d797f2
yuanxing <yuanxing@kylinsec.com.cn>
2025-04-29 11:55:04 +0800

    fix（pages）：modify LogListView::getLogList: logging instance runtimelogging ... - 完善调用后台接口文件

--- src/pages/audit/log-list/log-list-view.cpp
+++ src/pages/audit/log-list/log-list-view.cpp
@@ -182,7 +182,7 @@ void LogListView::getLogList(LogListPageType type, int page_on)
 
     request.set_page_no(page_on);
 
-    InfoWorker::getInstance().listRuntimeLogging(m_objId, request);
+    Logging::getInstance().listRuntimeLogging(m_objId, request);
 }
 
 void LogListView::onDateSelection()
