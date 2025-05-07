a85519e73da70c2c609ffeaadc544cba637171d2
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-07 10:19:10 +0800

    fix（common）：modify code changes - 调整common目录下的代码文件路径

--- src/common/config-group.cpp
+++ src/common/config-group.cpp
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/common/config-group.cpp
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
