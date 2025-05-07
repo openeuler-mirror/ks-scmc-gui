b27fcc0898ea4e40a441b8a7d0466646f20f4ab3
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-07 17:35:39 +0800

    fix（pages）：modify event timerevent user string - 添加用户管理功能和节点文件保护接口

--- src/pages/system/user-manager/user-list-page.h
+++ src/pages/system/user-manager/user-list-page.h
@@ -27,28 +27,39 @@ public:
     ~UserListPage();
     void updateInfo(QString keyword = "");  // 刷新表格
 
+protected:
+    virtual void timerEvent(QTimerEvent *event);
+
 private slots:
+    void search();
+    void refresh();
     void createUser();
     void deleteUsers();
-    void deleteUser(int row);
-    void editUser(int row);
+    void deleteUser(qint64 userID);
+    void editUser(qint64 userID, QString userName, qint64 roleID);
     void getListUserFinished(const QString objId, const QPair<grpc::Status, user::ListUserReply> &);
     void getCreateUserFinished(const QString objId, const QPair<grpc::Status, user::CreateUserReply> &);
     void getUpdateUserFinished(const QString objId, const QPair<grpc::Status, user::UpdateUserReply> &);
     void getRemoveUserFinished(const QString objId, const QPair<grpc::Status, user::RemoveUserReply> &);
+    void getListRoleFinished(const QString objId, const QPair<grpc::Status, user::ListRoleReply> &);
 
 private:
-    void initButtons();
-    void initTable();
+    void initUI();
     void initConnect();
+    void clearText();
+    void setTips(const QString &tips);
+    void getRoleList();
     UserUpdateDialog *popupDialog(DialogType type, const QString &title);
-    void getCheckedItemsId(QList<qint64> &ids);
     bool isSystemUser(const QString &userRole);
+    void updateDeleteBtnState(Qt::CheckState checkState);
 
 private:
+    Ui::UserListPage *ui;
     QString m_objID;
+    int m_timerID;
     UserUpdateDialog *m_createDialog;
     UserUpdateDialog *m_editDialog;
+    QMap<QString, qint64> m_roles;
 };
 
 #endif  // USERLISTPAGE_H
