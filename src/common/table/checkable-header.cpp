3f6882f81d8e3a9eae041d334f34c4934dcfb1c0
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-07 17:35:39 +0800

    fix（common）：add checkable header kiran log log i mouseevent - 添加用户管理功能和节点文件保护接口

---
+++ src/common/table/checkable-header.cpp
@@ -0,0 +1,67 @@
+#include "checkable-header.h"
+#include <kiran-log/qt5-log-i.h>
+#include <QMouseEvent>
+#include <QPainter>
+
+CheckableHeader::CheckableHeader(QWidget *parent) : QHeaderView(Qt::Horizontal, parent),
+                                                    m_stateChanged(false),
+                                                    m_checkState(Qt::Unchecked)
+{
+    m_rect = new QRect(13, 12, 16, 16);
+    // 做下拉筛选功能时可能会用到这个属性，暂设置为false
+    setSectionsClickable(false);
+    setMouseTracking(true);
+    // TODO: 使用setIndexWidget需要调整位置
+    // setIndexWidget(indexAt(QPoint(0, 0)), new QCheckBox(this));
+}
+
+void CheckableHeader::paintSection(QPainter *painter,
+                                   const QRect &rect,
+                                   int logicalIndex) const
+{
+    painter->save();
+    QHeaderView::paintSection(painter, rect, logicalIndex);
+    painter->restore();
+    if (logicalIndex == 0)
+    {
+        QPixmap pixmap;
+        switch (m_checkState)
+        {
+        case Qt::CheckState::Checked:
+            pixmap.load(":images/checked.svg");
+            break;
+        case Qt::CheckState::Unchecked:
+            pixmap.load(":images/check-default.svg");
+            break;
+        case Qt::CheckState::PartiallyChecked:
+            pixmap.load(":/images/checkbox-indeterminate-normal");
+            break;
+        default:
+            break;
+        }
+
+        style()->drawItemPixmap(painter, *m_rect, Qt::AlignCenter, pixmap);
+    }
+}
+
+void CheckableHeader::mousePressEvent(QMouseEvent *e)
+{
+    auto column = logicalIndexAt(e->pos());
+    if (column == 0)
+    {
+        if ((e->pos().x() > m_rect->x()) && (e->pos().y() > m_rect->y()) && (e->pos().x() < m_rect->x() + 16) && (e->pos().y() < m_rect->y() + 16))
+        {
+            m_checkState = m_checkState == Qt::Unchecked ? Qt::Checked : Qt::Unchecked;
+            emit toggled(m_checkState);
+            viewport()->update();
+        }
+    }
+    QHeaderView::mousePressEvent(e);
+}
+
+void CheckableHeader::setCheckState(Qt::CheckState checkState)
+{
+    m_stateChanged = true;
+    m_checkState = checkState;
+    viewport()->update();
+}
