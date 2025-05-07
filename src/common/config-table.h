f98fcbe4f2de01fd217b06cf0cdce1038c3ce964
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-07 10:19:10 +0800

    fix（common）：modify code changes - 调整common目录下的代码文件路径

--- src/common/config-table.h
+++ src/common/config-table.h
@@ -1,6 +1,6 @@
 /**
  * @file          /ks-scmc-gui/src/common/config-table.h
- * @brief         
+ * @brief
  * @author        yuanxing <yuanxing@kylinos.com>
  * @copyright (c) 2022 KylinSec. All rights reserved.
  */
