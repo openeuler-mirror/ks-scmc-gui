41af4177398c656e1d7609ab452a4cbbb32cda29
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-08 18:29:22 +0800

    fix（common）：add statechanged checkstate state - 完善用户管理功能

--- src/common/table/checkable-header.h
+++ src/common/table/checkable-header.h
@@ -19,6 +19,7 @@ protected:
 
 signals:
     void toggled(Qt::CheckState checkState);
+    void checkStateChanged(Qt::CheckState checkState);
 
 public slots:
     void setCheckState(Qt::CheckState checkState);
