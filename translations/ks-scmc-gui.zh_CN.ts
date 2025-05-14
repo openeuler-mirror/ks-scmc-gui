8797895d07f434aaf8732200f688595d1dd2c8b7
yuanxing <yuanxing@kylinsec.com.cn>
2025-05-14 16:53:36 +0800

    fix（translations）：modify container manager container app page - 修复sonarqube扫描问题

--- translations/ks-scmc-gui.zh_CN.ts
+++ translations/ks-scmc-gui.zh_CN.ts
@@ -375,7 +375,7 @@
 <context>
     <name>ContainerAppDialog</name>
     <message>
-        <location filename="../src/pages/container/container-manager/container-app-page.cpp" line="598"/>
+        <location filename="../src/pages/container/container-manager/container-app-page.cpp" line="596"/>
         <source>Container app</source>
         <translation>容器应用</translation>
     </message>
